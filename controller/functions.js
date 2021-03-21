40469c7009b2881a5d3b8cb324c7f0a867985a95
faypaym <benoit.vandamme@laposte.net>
2021-03-21 14:28:36 +0100

    connexion bdd

---
+++ controller/functions.js
@@ -0,0 +1,29 @@
+
+
+const admin = require('firebase-admin');
+
+
+
+var serviceAccount = require("./anniv-app-f859f-firebase-adminsdk-ws4v8-00e0b25aa5.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://anniv-app-f859f-default-rtdb.firebaseio.com/"
+});
+
+
+
+
+
+const db = admin.firestore();
+
+
+function getQuote() {
+  const quoteData = {
+  quote: "random5",
+  author: "String"
+  };
+  return db.collection("sampleData").doc("inspiration").set(quoteData).then(() => {
+  console.log("new quote was written to the database");})
+  }
+  
+  getQuote();
